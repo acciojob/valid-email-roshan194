e206958cf8c49ffb5a0eedcf2ed60fc716d72bae
Roshan <71066704+roshan194@users.noreply.github.com>
2025-04-05 12:25:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 function validEmail(str) {
-  //your JS code here.
-}
+  if (!str) return false;
 
-// Do not change the code below.
+  const emailRegex = /^[a-zA-Z0-9]+([._-]?[a-zA-Z0-9]+)*@[a-zA-Z0-9]+([.-]?[a-zA-Z0-9]+)*\.[a-zA-Z]{2,}$/;
+
+  return emailRegex.test(str);
+}
 const str = prompt("Enter an email address.");
 alert(validEmail(str));
